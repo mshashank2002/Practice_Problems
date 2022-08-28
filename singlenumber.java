be747719516e8b3d2afceb5ac075c0db5dc34ace
M Shashank <103320850+mshashank2002@users.noreply.github.com>
2022-08-28 19:32:18 +0530

    Create singlenumber.java

---
+++ singlenumber.java
@@ -0,0 +1,19 @@
+/*Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.*/
+
+
+class Solution {
+    public int singleNumber(int[] nums)
+    {
+        Arrays.sort(nums);
+        int i;
+        for(i=nums.length-1;i>0;i-=2)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                break;
+            }
+        }
+        return nums[i];
+        
+    }
+}
