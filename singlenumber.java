3ee6e05a53e17fb72cfac2baabee25ac015398aa
mshashank2002 <mshashank8867@gmail.com>
2022-11-02 19:31:55 +0530

    Solution Added

--- singlenumber.java
+++ singlenumber.java
@@ -1,6 +1,6 @@
 /*Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.*/
 
-
+import java.util.*;
 class Solution {
     public int singleNumber(int[] nums)
     {
