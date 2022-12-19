248efc0328ebc76a9de7e68af3f15ca628cb233e
M Shashank <103320850+mshashank2002@users.noreply.github.com>
2022-12-19 20:00:44 +0530

    Create more_than_once.java
    Signed-off-by: M Shashank <103320850+mshashank2002@users.noreply.github.com>

---
+++ Find the element that appears once - GFG/more_than_once.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            
+            for (int i = 0; i < n; ++i)
+                arr[i] = sc.nextInt ();
+
+    		System.out.println (new Sol().search (arr, n));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    public static int search(int A[], int N)
+    {
+        int i=N-1,ele=A[0];
+        while(i>0)
+        {
+            if(A[i]==A[i-1])
+            {
+                i-=2;
+            }
+            else
+            {
+                ele=A[i];
+                break;
+            }
+        }
+        return ele;
+    }
+}
