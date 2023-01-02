6a7e0ded0108118e3553567033f75ffc3624a7ff
M Shashank <103320850+mshashank2002@users.noreply.github.com>
2023-01-02 22:52:24 +0530

    Added solution - LeetHub

---
+++ 3 sum closest - GFG/3-sum-closest.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int Arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                Arr[i] = Integer.parseInt(input_line[i]);
+            int X = Integer.parseInt(read.readLine());
+            
+            Solution ob = new Solution();
+            System.out.println(ob.closest3Sum(Arr, N, X));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int closest3Sum(int A[], int N, int X)
+    {
+        Arrays.sort(A);
+        int min=Integer.MAX_VALUE;
+        int j,k;
+        int ans=A[0];
+        for(int i=0;i<N-2;i++)
+        {
+            j=i+1;
+            k=N-1;
+            while(j<k)
+            {
+                 int sum=A[i]+A[j]+A[k];
+                if(Math.abs(X-sum)<min)
+                {
+                    min=Math.abs(X-sum);
+                    ans=sum;
+                    
+                }
+                else if(sum==X)
+                {
+                    return X;
+                }
+                else if(sum>X)
+                {
+                    k--;
+                }
+                else 
+                {
+                    j++;
+                    
+                }
+                
+            }
+        }
+        return ans;
+        
+    }
+}
