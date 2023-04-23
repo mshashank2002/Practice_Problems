bc2694faefe31113873c0a8601b2c7bfaf134177
M Shashank <103320850+mshashank2002@users.noreply.github.com>
2023-04-23 19:15:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Max Sum without Adjacents/max-sum-without-adjacents.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMaxSum(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int findMaxSum(int arr[], int n)
+    {
+        int prev2 = 0;
+        int prev1 = arr[0];
+        
+        for(int i = 1; i < n; i++){
+            int pick = arr[i] + prev2;
+            int notPick = 0 + prev1;
+            int curr = Math.max(pick, notPick);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return prev1;
+       
+       
+    }
+}
