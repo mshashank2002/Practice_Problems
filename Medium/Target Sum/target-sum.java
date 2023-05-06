8d920c06065f34046867837e9ee3893af24a783f
M Shashank <103320850+mshashank2002@users.noreply.github.com>
2023-05-06 18:53:31 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Target Sum/target-sum.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+//Initial Template for JAVA
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            
+            int[] A = new int[N];
+            
+            for(int i=0 ; i<N ; i++){
+                A[i] = Integer.parseInt(S[i]);
+            }
+            int target = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.findTargetSumWays(A,N, target));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int findTargetSumWays(int[] arr , int n, int target) 
+    {
+         int totSum = 0;
+    for(int i=0; i<arr.length;i++){
+        totSum += arr[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-target<0) return 0;
+    if((totSum-target)%2==1) return 0;
+    
+    int s2 = (totSum-target)/2;
+    
+    int dp[][]=new int[n][s2+1];
+    for(int row[]: dp)
+    Arrays.fill(row,-1);
+    return countPartitionsUtil(n-1,s2,arr,dp);
+       
+    }
+    static int countPartitionsUtil(int ind, int target, int[] arr,int[][] dp){
+
+     if(ind == 0){
+        if(target==0 && arr[0]==0)
+            return 2;
+        if(target==0 || target == arr[0])
+            return 1;
+        return 0;
+    }
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    int notTaken = countPartitionsUtil(ind-1,target,arr,dp);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = countPartitionsUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= (notTaken + taken);
+}
+    
+};
