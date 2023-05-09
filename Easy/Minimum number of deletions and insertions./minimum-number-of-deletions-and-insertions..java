c23fc5ab1a063111510ab82c5fc688b495a38117
M Shashank <103320850+mshashank2002@users.noreply.github.com>
2023-05-09 13:46:22 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum number of deletions and insertions./minimum-number-of-deletions-and-insertions..java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s1 = sc.next();
+                    String s2 = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minOperations(s1,s2));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+	public int minOperations(String str1, String str2) 
+	{ 
+	    int n=str1.length();
+	    int m=str2.length();
+	    int k=lcs(str1,str2);
+	    return (n-k)+(m-k);
+	} 
+	static int lcs(String s1,String s2)
+	{
+	int n=s1.length();
+    int m=s2.length();
+
+    int dp[][]=new int[n+1][m+1];
+    for(int rows[]:dp)
+    Arrays.fill(rows,-1);
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 0;
+    }
+    for(int i=0;i<=m;i++){
+        dp[0][i] = 0;
+    }
+    
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1.charAt(ind1-1)==s2.charAt(ind2-1))
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+            else
+                dp[ind1][ind2] = 0 + Math.max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+        }
+    }
+    
+    return dp[n][m];
+}
+}
