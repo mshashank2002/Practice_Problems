f91e38a1c7e09387fa8183175ca1009a1d023774
M Shashank <103320850+mshashank2002@users.noreply.github.com>
2023-06-07 20:35:48 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Printing Longest Increasing Subsequence/printing-longest-increasing-subsequence.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG{
+    public static void main(String args[]) throws IOException{
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int N=sc.nextInt();
+            int []arr=new int[N];
+            for(int i=0;i<N;i++){
+                arr[i]=sc.nextInt();
+            }
+            Solution obj=new Solution();
+            ArrayList<Integer>ans=obj.longestIncreasingSubsequence(N, arr);
+            for(int i=0;i<ans.size();i++){
+                System.out.print(ans.get(i)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public ArrayList<Integer> longestIncreasingSubsequence(int n, int arr[]){
+          int[] dp=new int[n];
+    Arrays.fill(dp,1);
+    int[] hash=new int[n];
+    Arrays.fill(hash,1);
+    
+    for(int i=0; i<=n-1; i++){
+        
+        hash[i] = i; // initializing with current index
+        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+            
+            if(arr[prev_index]<arr[i] && 1 + dp[prev_index] > dp[i]){
+                dp[i] = 1 + dp[prev_index];
+                hash[i] = prev_index;
+            }
+        }
+    }
+    
+    int ans = -1;
+    int lastIndex =-1;
+    
+    for(int i=0; i<=n-1; i++){
+        if(dp[i]> ans){
+            ans = dp[i];
+            lastIndex = i;
+        }
+    }
+    
+    ArrayList<Integer> temp=new ArrayList<>();
+    temp.add(arr[lastIndex]);
+    
+    while(hash[lastIndex] != lastIndex){ // till not reach the initialization value
+        lastIndex = hash[lastIndex];
+        temp.add(arr[lastIndex]);    
+    }
+    Collections.reverse(temp);
+    return temp;
+    }
+}
